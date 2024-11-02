0dfabfe29e83111c0331645fe4c24292c980a432
unknown <liashchenkomarina@gmal.com>
2024-11-02 16:55:28 +0100

    add files

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
-
+import ContactsForm from './components/ContactsForm';
+import ContactList from './components/ContactList';
+import SearchBox from './components/SearchBox';
 import './App.css'
 
-function App() {
+const App = () => {
+    return (
+        <div>
+            <h1>Phonebook</h1>
+            <ContactsForm />
 
+            <h2>Contacts</h2>
+            <SearchBox />
+            <ContactList />
+        </div>
+    )
 }
 
 export default App
---
+++ src/components/Contact.jsx
@@ -0,0 +1,14 @@
+import { useDispatch } from "react-redux"
+import { deleteContact } from '../redux/contactsSlice'
+
+
+const Contact = ({ id, name, number }) => {
+    const dispatch = useDispatch();
+    return (
+        <li>
+            {name}: {number}
+            <button onClick={() => dispatch(deleteContact(id))}>Delete</button>
+        </li>
+    )
+};
+export default Contact; 
